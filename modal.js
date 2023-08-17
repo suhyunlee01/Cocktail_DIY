bd80d1878b5117dda3d02e5a5cb63e55043fc983
suhyunlee <118110277+suhyunlee01@users.noreply.github.com>
2023-08-17 22:22:59 +0900

    Add LogIn Modal

---
+++ modal.js
@@ -0,0 +1,33 @@
+let signinbtn = document.querySelector(".sign_in");
+let modalWrap = document.querySelector(".modalWrap");
+let closeModal = document.querySelector(".close_modal");
+let goSignUP = document.querySelector(".modalBody_signup_signuppage");
+let goSignIn = document.querySelector(".modalBody_signin_signuppage");
+let modaltext = document.querySelector(".modalBody_header_text");
+let modalSubmit = document.querySelector(".modalBody_form_submit");
+
+function modalHandler(){
+    modalWrap.style.display="flex";
+}
+signinbtn.addEventListener("click", modalHandler);
+
+function closeModalHandler(){
+    modalWrap.style.display="none";
+}
+closeModal.addEventListener("click", closeModalHandler);
+
+function singUpModal(){
+    modaltext.innerHTML="Sign Up"
+    goSignUP.style.backgroundColor = "#0D7D0B";
+    goSignIn.style.backgroundColor = "#CCC";
+    modalSubmit.value = "Sign Up";
+}
+goSignUP.addEventListener("click",singUpModal);
+
+function singInModal(){
+    modaltext.innerHTML="Sign In"
+    goSignIn.style.backgroundColor = "#0D7D0B";
+    goSignUP.style.backgroundColor = "#CCC";
+    modalSubmit.value = "Sign In";
+}
+goSignIn.addEventListener("click",singInModal);
