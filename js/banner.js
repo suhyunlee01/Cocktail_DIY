ea428b589a2a19835f1f4ee774fd85f90d9d001a
suhyunlee <118110277+suhyunlee01@users.noreply.github.com>
2023-09-13 16:38:03 +0900

    Feat: 배너 애니메이션 추가

--- js/banner.js
+++ js/banner.js
@@ -1,28 +1,20 @@
-
-window.onscroll = ()=>{
-    const img = document.querySelector('.banner_content_img');
-    const vH = window.innerHeight;
-    const vhToImg = img.getBoundingClientRect().top;
-    const scrollLength = vH - vhToImg;
-    const imgHeight = img.clientHeight;
-    let scrollRate = scrollLength/imgHeight * 100;
+window.addEventListener('scroll', function() {
+    const img = document.querySelector('.banner_content_img_src');
+    const imgContainer = document.querySelector('.banner_content_img');
     
-    if(scrollLength/imgHeight * 100 <0){
-        scrollRate = 0;
-    }else if(scrollLength/imgHeight * 100 > 100){
-        scrollRate = 100;
+    // 이미지가 화면에 보일 때 fade in 효과 적용
+    if (isInViewport(imgContainer)) {
+        img.style.opacity = 1;
     }
-    
-    console.log(scrollRate);
+});
 
-    const imgSRC = document.querySelector('.banner_content_img_src');
-    if(scrollRate == 10){
-        imgSRC.style.opacity = "0.4";
-    }else if(scrollRate == 80){
-        imgSRC.style.opacity = "0.8";
-    }else if(scrollRate == 90){
-        imgSRC.style.opacity = "0.9";
-    }else if(scrollRate == 100){
-        imgSRC.style.opacity = "1";
-    }
+// 요소가 화면에 보이는지 확인하는 함수
+function isInViewport(imgContainer) {
+    const rect = imgContainer.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
 }
