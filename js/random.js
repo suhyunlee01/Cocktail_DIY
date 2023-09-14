c53f77db4e8de48993d3d70a2a400fba6449d903
suhyunlee <118110277+suhyunlee01@users.noreply.github.com>
2023-09-14 15:42:10 +0900

    Add: 랜덤 모달 null값 예외처리

--- js/random.js
+++ js/random.js
@@ -47,10 +47,7 @@ function generateRandomModal(randomDrink){
                         <div class="recipe_modal_body_ingredient">
                             <span class="recipe_modal_body_header">Ingredient</span>
                             <div class="recipe_modal_body_text">
-                                <ul class="ingredients">
-                                <li>${randomDrink.strIngredient1}${randomDrink.strMeasure1}</li>
-                                <li>${randomDrink.strIngredient2}${randomDrink.strMeasure2}</li>
-
+                                <ul id="ingredient_s">
                                 </ul>
                             </div>
                         </div>
@@ -66,7 +63,33 @@ function generateRandomModal(randomDrink){
                         </div>
                     </div>
                 </div>`;
+      //ingredient null값 제거하고 html 할당하는 함수 호출
+      generateingredients(randomDrink);
+    }
+
+
+//ingredient null값 제거하고 html 할당하는 함수
+function generateingredients(randomDrink) {
+    console.log("Data in generateingredient:", randomDrink);
+
+    let ingredientsList = "";
+
+    for (let i = 1; i <= 15; i++) {
+        const ingredient = randomDrink[`strIngredient${i}`]; //[key]
+        const measure = randomDrink[`strMeasure${i}`];
+
+        //ingredient랑 measure이 null 아닐 때
+        if (ingredient && measure) {
+            ingredientsList += `<li>${ingredient} ${measure}</li>`;
+        } else {
+            break; // 재료나 양이 없을 경우 반복문 종료
+        }
+    }
+
+    document.getElementById("ingredient_s").innerHTML = ingredientsList;
 }
+
+
 
 
 //윈도우 로드 시 이벤트 핸들러
