fb7b0c9097e5393cc7a46def49e2bfdb4436ca30
suhyunlee <118110277+suhyunlee01@users.noreply.github.com>
2023-10-08 23:13:28 +0900

    Feat: 배너 애니메이션 추가

--- js/banner.js
+++ js/banner.js
@@ -1,20 +1,41 @@
-window.addEventListener('scroll', function() {
-    const img = document.querySelector('.banner_content_img_src');
-    const imgContainer = document.querySelector('.banner_content_img');
-    
-    // 이미지가 화면에 보일 때 fade in 효과 적용
-    if (isInViewport(imgContainer)) {
-        img.style.opacity = 1;
-    }
-});
+const imgContainer = document.querySelector('.banner_content_img');
+const bannerText = document.querySelector('.banner_content_txt');
 
-// 요소가 화면에 보이는지 확인하는 함수
-function isInViewport(imgContainer) {
-    const rect = imgContainer.getBoundingClientRect();
-    return (
-        rect.top >= 0 &&
-        rect.left >= 0 &&
-        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-    );
+function bannerAnimate(imgContainer) {
+    let observer = new IntersectionObserver((entries)=>{
+        //감시중인 요소들이 담긴 entries 배열 순회하기
+        entries.forEach((entry)=>{
+            //감시중인 요소에 isIntersecting 속성이 true 반환한 경우
+            if(entry.isIntersecting===true){
+                console.log("isIntersecting true 반환");
+
+                // 보이면 hide에서 show 클래스 추가
+                imgContainer.classList.add('show');
+            }else{
+                //안보이면 다시 show 클래스 제거
+                imgContainer.classList.remove('show');
+            }
+        })
+    })
+    observer.observe(imgContainer);
 }
+
+function bannerTextAnimate(bannerText){
+    let observer = new IntersectionObserver((entries)=>{
+        entries.forEach((entry)=>{
+            if(entry.isIntersecting===true){
+                console.log("isIntersecting true 반환");
+
+                // 보이면 hide에서 show 클래스 추가
+                bannerText.classList.add('show2');
+            }else{
+                //안보이면 다시 show 클래스 제거
+                bannerText.classList.remove('show2');
+            }
+        })
+    })
+    observer.observe(bannerText);
+}
+
+bannerTextAnimate(bannerText);
+bannerAnimate(imgContainer);
